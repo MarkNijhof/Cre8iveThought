22adbf6a297dde417a8a2440a2d68abc6c833e7a
Mark Nijhof <Mark.Nijhof@Gmail.com>
2010-05-27 14:34:17 +0200

    initial site

---
+++ public/javascripts/json.js
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title xmlns="http://www.google.com/ns/jotspot">Download Attachment</title>
+<style xmlns="http://www.google.com/ns/jotspot">
+          html, body {
+            padding: 0;
+            margin: 0;
+          }          
+          body {
+            font-family: Arial, Verdana, sans-serif;
+            font-size: 12px;
+            background-color: #fff;
+            background-image: none;
+            direction: ltr;
+          }
+          .sites-clear:after {
+            content: ".";
+            display: block;
+            height: 0;
+            clear: both;
+            visibility: hidden;
+          }
+          #sites-header {
+            background-color: #fff;
+            border-bottom: 1px solid #b0bdcc;
+            height: 1%;
+            padding: 5px 5px 0 0;
+            margin-bottom: 10px;
+          }
+          .sites-account {
+            float: right;
+            font-size: 12px;
+            padding: 4px 8px 5px;
+          }
+          .sites-account a {
+            color: #4e7dbf; /* onebarLinkColor */
+          }
+          .sites-content-wrapper {
+            min-height: 150px;
+            margin: 0 5px;
+            _height: 150px !important;
+            padding: 5px;
+          }
+          h2 {
+            margin: 5px 0 10px;
+            font-size: 22px;
+          }
+          p {
+            margin-top: 0px;
+          }
+          .sites-subfooter {
+            font-size: 11px;
+            padding: 5px 0 5px;
+            text-align: center;
+          }
+          .sites-subfooter p {
+            margin: 0;
+            padding: 5px;
+          }
+          .sites-subfooter p span {
+            font-size: 10px;
+          }
+          #sites-logo {
+            margin-bottom: 15px;
+          }
+        </style>
+</head>
+<body xmlns="http://www.google.com/ns/jotspot">
+<div id="sites-status" class="sites-status" style="display:none;">
+<div id="sites-notice" class="sites-notice"> </div>
+</div>
+<div class="sites-content-wrapper">
+<img xmlns="http://www.w3.org/1999/xhtml" src="http://www.gstatic.com/sites/p/3c554e/system/app/images/logo/logo.gif" id="sites-logo" alt="Google Sites Logo" />
+<h2>Download Attachment</h2>
+<p xmlns="http://www.w3.org/1999/xhtml">
+        Click <a href="/site/jollytoad/json.js?attredirects=0">here</a> to download your attachment.
+        </p>
+</div>
+</body>
+</html>
