7d9cbcd3f8e7d28cc6fca9494c7321d74b865bec
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-01-27 01:12:41 +0100

    made a first attempt to fix header issues in Opera, dissapointing behavior

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -1,23 +1,41 @@
 
 $(window).scroll(function () {  
-	var scrollTop = 5;
+	var scrollTop = 15;
 	var topSpaceTop = '0px'
 
-	if ($(window).scrollTop() > scrollTop) {
-		if ($('#profile-picture').css('width') == '222px') {
-			$('#profile-picture').animate({
-				width: 0
-			}, 200);
-		}	
+	if ($(window).scrollTop() >= scrollTop) {
+		hide_profile_picture();
 	} else {
-		if ($('#profile-picture').css('width') == '0px') {
-			$('#profile-picture').animate({
-				width: 222
-			}, 500);
-		}
+		show_profile_picture();
 	}
-});  
+});
 
-$(document).ready(function(){  
+var is_animating_profile_picture = false;
+
+function show_profile_picture(){
+	if ($('#profile-picture').css('width') == '0px' && !is_animating_profile_picture) {
+		is_animating_profile_picture = true;
+		$('#profile-picture').animate({
+			width: 222
+		}, 500, function(){	
+			is_animating_profile_picture = false;
+		});
+	}	
+}
+
+function hide_profile_picture(){
+	if ($('#profile-picture').css('width') == '222px' && !is_animating_profile_picture) {
+		is_animating_profile_picture = true;
+		$('#profile-picture').animate({
+			width: 0
+		}, 200, function(){	
+			is_animating_profile_picture = false;
+		});
+	}	
+}
+
+
+
+$(document).ready(function(){
 	$('#footer').css('margin-bottom', $(window).height() - 500 + 'px');
 });
