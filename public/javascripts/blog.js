e1953b0f1b05923fc1267d2caa237bfa1e405017
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-02-02 01:43:01 +0100

    changed the annimation to react to half way changes

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -1,40 +1,31 @@
 
-$(window).scroll(function () {  
-	var scrollTop = 15;
-	var topSpaceTop = '0px'
+var scroll_top = 15;
+var to_show_the_profile_picture = true;
+var to_hide_the_profile_picture = false;
 
-	if ($(window).scrollTop() >= scrollTop) {
-		hide_profile_picture();
-	} else {
-		show_profile_picture();
-	}
+$(window).scroll(function () {  
+	check_if_an_annimation_is_needed();
 });
 
-var is_animating_profile_picture = false;
-
-function show_profile_picture(){
-	if ($('#profile-picture').css('width') == '0px' && !is_animating_profile_picture) {
-		is_animating_profile_picture = true;
-		$('#profile-picture').animate({
-			width: 222
-		}, 500, function(){	
-			is_animating_profile_picture = false;
-		});
-	}	
+function check_if_an_annimation_is_needed(){
+	if (is_currently_below_scroll_top()) {
+		start_annimation(to_hide_the_profile_picture);
+	} else {
+		start_annimation(to_show_the_profile_picture);
+	}
 }
 
-function hide_profile_picture(){
-	if ($('#profile-picture').css('width') == '222px' && !is_animating_profile_picture) {
-		is_animating_profile_picture = true;
-		$('#profile-picture').animate({
-			width: 0
-		}, 200, function(){	
-			is_animating_profile_picture = false;
-		});
-	}	
+function start_annimation(should_the_profile_picture_be_shown){
+	if (should_the_profile_picture_be_shown){
+		$('#profile-picture').stop().animate({ width: 222 }, 1000);
+	} else {
+		$('#profile-picture').stop().animate({ width: 0 }, 300);
+	}
 }
 
-
+function is_currently_below_scroll_top(){
+	return $(window).scrollTop() >= scroll_top;
+}
 
 $(document).ready(function(){
 	$('#footer').css('margin-bottom', $(window).height() - 500 + 'px');
